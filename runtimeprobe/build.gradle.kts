99c0a37df395dea130ce12ea3f71cb1569aa3605
Ffenuss <78445717+Ffenuss@users.noreply.github.com>
2026-09-20 00:41:34 +0300

    build: compile runtime probe native helper for four ABIs

--- runtimeprobe/build.gradle.kts
+++ runtimeprobe/build.gradle.kts
@@ -4,6 +4,7 @@ plugins {
 
 android {
     namespace = "io.github.ffenuss.modkit.runtimeprobe"
+    ndkVersion = "28.2.13676358"
     compileSdk {
         version = release(37) {
             minorApiLevel = 0
@@ -16,11 +17,26 @@ android {
         targetSdk = 36
         versionCode = 1
         versionName = "1"
+
+        ndk {
+            abiFilters += listOf(
+                "arm64-v8a",
+                "armeabi-v7a",
+                "x86",
+                "x86_64",
+            )
+        }
     }
 
     buildTypes {
         release {
             isMinifyEnabled = false
+        }
+    }
+
+    externalNativeBuild {
+        ndkBuild {
+            path = file("src/main/jni/Android.mk")
         }
     }
 
