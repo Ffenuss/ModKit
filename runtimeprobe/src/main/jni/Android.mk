94c71fb416bfaf068fcc13ec453ec2f128e6abdd
Ffenuss <78445717+Ffenuss@users.noreply.github.com>
2026-09-23 19:10:31 +0300

    Remove root native helper targets

--- runtimeprobe/src/main/jni/Android.mk
+++ runtimeprobe/src/main/jni/Android.mk
@@ -6,18 +6,3 @@ LOCAL_SRC_FILES := runtime_native_probe.c
 LOCAL_CFLAGS := -std=c11 -Wall -Wextra -Werror
 LOCAL_LDLIBS := -ldl
 include $(BUILD_SHARED_LIBRARY)
-
-ifeq ($(TARGET_ARCH_ABI),arm64-v8a)
-include $(CLEAR_VARS)
-LOCAL_MODULE := modkit_root_memory_watch
-LOCAL_SRC_FILES := root_memory_watch.c
-LOCAL_CFLAGS := -std=c11 -Wall -Wextra -Werror
-include $(BUILD_EXECUTABLE)
-endif
-
-
-include $(CLEAR_VARS)
-LOCAL_MODULE := modkit_root_fast_value_scan
-LOCAL_SRC_FILES := root_fast_value_scan.c
-LOCAL_CFLAGS := -std=c11 -Wall -Wextra -Werror
-include $(BUILD_EXECUTABLE)
