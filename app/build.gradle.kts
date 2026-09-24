496e389fd94df08392f12dd99e3f2f751c838c8c
Ffenuss <78445717+Ffenuss@users.noreply.github.com>
2026-09-24 09:08:18 +0300

    Bump APK version to 0.0.20

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,8 +26,8 @@ android {
         applicationId = "io.github.ffenuss.modkit"
         minSdk = 26
         targetSdk = 36
-        versionCode = 19
-        versionName = "0.0.19"
+        versionCode = 20
+        versionName = "0.0.20"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
