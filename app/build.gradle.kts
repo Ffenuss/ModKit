a0297f879da4b3ddaaa0b9e3c35853582a4e284d
Ffenuss <78445717+Ffenuss@users.noreply.github.com>
2026-09-24 18:19:05 +0300

    Bump ModKit to 0.0.25

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,8 +26,8 @@ android {
         applicationId = "io.github.ffenuss.modkit"
         minSdk = 26
         targetSdk = 36
-        versionCode = 24
-        versionName = "0.0.24"
+        versionCode = 25
+        versionName = "0.0.25"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
